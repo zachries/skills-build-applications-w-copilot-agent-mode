2a60501c73c31814ec1686854735a8412111408b
zachries <48293320+zachries@users.noreply.github.com>
2025-04-25 18:56:54 +0000

    Check again

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "djongo2",
+        "ENGINE": "djongo",
         "NAME": "octofit_db",
         "HOST": "localhost",
         "PORT": 27017,
