d5c1df6257e746f69aca8790c8b62bf5feea4953
zachries <48293320+zachries@users.noreply.github.com>
2025-04-25 18:56:14 +0000

    Retrigger the check

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "djongo",
+        "ENGINE": "djongo2",
         "NAME": "octofit_db",
         "HOST": "localhost",
         "PORT": 27017,
